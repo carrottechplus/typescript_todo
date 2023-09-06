56a110a2bf59b32c4360d682b626715dbb82f455
carrottechplus <blohzzang@gmail.com>
2023-09-06 12:07:56 +0900

    리스트를 새로 추가할때마다 로컬저장소 변경하고 목록 다시 생성

--- test.js
+++ test.js
@@ -1,8 +1,13 @@
 const form = document.querySelector('#form');
 const input = document.querySelector('#title');
 const list = document.querySelector('#list');
-//할일 목록이 저장될 배열 생성
-let tasks = [];
+// 처음 페이지 로딩시 로컬저장소에서 TASKS에 대한 데이터 호출
+let data = localStorage.getItem('TASKS');
+// 해당 데이터가 있으면 parsing해서 tasks배열에 저장, 없으면 빈배열
+let tasks = data ? JSON.parse(data) : [];
+
+//tasks에 배열값을 반복출력 (만약 저장소에 값이 없으면 출력안됨)
+tasks.map((task) => addListItem(task));
 
 //폼요소에 submit이벤트 연결
 form.addEventListener('submit', (e) => {
@@ -25,6 +30,9 @@ form.addEventListener('submit', (e) => {
 
 	// console.log(tasks);
 	list.innerHTML = '';
+	// 새로운 객체가 만들어지면 저장소에 데이터를 집어넣고
+	localStorage.setItem('TASKS', JSON.stringify(tasks));
+	// tasks에 있는 배열값을 반복돌면서 목록 생성
 	tasks.map((task) => addListItem(task));
 });
 
@@ -34,12 +42,18 @@ function addListItem(task) {
 	const checkbox = document.createElement('input');
 	checkbox.type = 'checkbox';
 
+	// 동적으로 추가된 상태에서 새로고침해도 취소선과 체크상태를 유지
+	checkbox.checked = task.complete ? true : false;
+	item.style.textDecoration = task.complete ? 'line-through' : 'none';
+
 	// 동적으로 생성되는 체크박스 요소에 아예 이벤트 핸들러까지 연결해서 생성
 	// 이벤트위임을 하지 않아도 동적인 요소에 이벤트 연결하는 방법
 	checkbox.addEventListener('change', () => {
 		task.complete = checkbox.checked;
 		item.style.textDecoration = task.complete ? 'line-through' : 'none';
-		console.log(tasks);
+
+		// 동적으로 생긴 체크박스 요소에 change이벤트가 발생할 때 마다 다시 변경점을 로컬저장소에 저장
+		localStorage.setItem('TASKS', JSON.stringify(tasks));
 	});
 
 	item.append(checkbox, task.title);
